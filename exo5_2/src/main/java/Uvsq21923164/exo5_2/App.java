ac0f7ad24e1fa24ee71c8a0edf445894c02fe87e
KEBIR Ramdane <ramdane.kebir@outlook.fr>
2020-04-22 13:17:17 +0200

    creation projet maven

---
+++ exo5_2/src/main/java/Uvsq21923164/exo5_2/App.java
@@ -0,0 +1,13 @@
+package Uvsq21923164.exo5_2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
