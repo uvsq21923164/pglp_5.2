a582c76aeb6e4067b7a40384db62aff30963b406
KEBIR Ramdane <ramdane.kebir@outlook.fr>
2020-04-30 01:59:04 +0200

    modifier la class AbstractDAOFactory

--- exo5_2/src/main/java/Uvsq21923164/exo5_2/AbstractDAOFactory.java
+++ exo5_2/src/main/java/Uvsq21923164/exo5_2/AbstractDAOFactory.java
@@ -1,17 +1,16 @@
 package Uvsq21923164.exo5_2;
 
 public abstract class AbstractDAOFactory {
-	
-public enum DAOType { Serial};
+	public enum DAOType {JDBC, Serial};
 	
 	public abstract DAO<Personnel> getPersonnelDAO();
 	
 	public abstract DAO<PersonnelGroupe> getPersonnelGroupeDAO();
 	
 	public static AbstractDAOFactory getFactory(DAOType type) {
-		
+		if (type == DAOType.JDBC) return new JdbcDAOFactory();
 		if (type == DAOType.Serial) return new DAOFactory();
 		return null;
 	}
-
+	
 }
