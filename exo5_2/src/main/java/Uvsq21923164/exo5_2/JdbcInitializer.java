a03380105720bf72b2569c3dba8c995a7d03af48
KEBIR Ramdane <ramdane.kebir@outlook.fr>
2020-05-07 01:59:30 +0200

    modifier la classe

--- exo5_2/src/main/java/Uvsq21923164/exo5_2/JdbcInitializer.java
+++ exo5_2/src/main/java/Uvsq21923164/exo5_2/JdbcInitializer.java
@@ -6,6 +6,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
 
+
 public class JdbcInitializer {
 	
 	private static final String userName = "ramdane";
@@ -18,14 +19,15 @@ public class JdbcInitializer {
 		connectionProps.put("user", userName);
 		connectionProps.put("user", password);
 	}
-
+/*  methode qui supprime la table s'il exit ensuit cree une nouvelle table */
+	
 	public void dropCreateTables() {
 		try (Connection connect = DriverManager.getConnection(dburl)) {
 			
 			Statement state = connect.createStatement();
-			/*state.addBatch("DROP TABLE personnelGroupes");
+			state.addBatch("DROP TABLE personnelGroupes");
 			state.addBatch("DROP TABLE appartient");
-			state.addBatch("DROP TABLE personnels");*/
+			state.addBatch("DROP TABLE personnels");
 			state.addBatch("CREATE TABLE personnelGroupes ("
 					+ "id VARCHAR(100) PRIMARY KEY"
 					+ ")");
@@ -48,28 +50,5 @@ public class JdbcInitializer {
 		}	
 	}
 	
-	public void createTables() {
-		try (Connection connect = DriverManager.getConnection(dburl)) {
-			Statement state = connect.createStatement();
-			state.addBatch("CREATE TABLE personnelGroupes ("
-					+ "id VARCHAR(100) PRIMARY KEY"
-					+ ")");
-			state.addBatch( // Association n-n
-					"CREATE TABLE appartient ("
-					+ "id VARCHAR(100),"
-					+ "nom VARCHAR(100)"
-					+ ")"); // Clés étrangères non définies
-
-			state.addBatch(
-					"CREATE TABLE personnels ("
-					+ "nom VARCHAR(100) PRIMARY KEY,"
-					+ "prenom VARCHAR(100) NOT NULL,"
-					+ "fonction VARCHAR(100) NOT NULL"
-					// + "date_NAISSANCE DATE"
-					+ ")");
-			state.executeBatch();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}		
-	}
+	
 }
