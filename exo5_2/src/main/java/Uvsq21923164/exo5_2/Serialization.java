4a1119962b6881c3a6256db8a66684001d248610
KEBIR Ramdane <ramdane.kebir@outlook.fr>
2020-05-07 02:02:09 +0200

    modifier la classe serialisation

--- exo5_2/src/main/java/Uvsq21923164/exo5_2/Serialization.java
+++ exo5_2/src/main/java/Uvsq21923164/exo5_2/Serialization.java
@@ -4,15 +4,37 @@ import Uvsq21923164.exo5_2.AbstractDAOFactory.DAOType;
 import Uvsq21923164.exo5_2.Personnel.PersonnelBuilder;
 
 public enum Serialization {
-
-	   APPLICATION;
-
-	public void run(final String[] args) {
-		
+	ENVIRONNEMENT;
+	
+	private void run(String[] args, DAOType dt) {
+		DAO<Personnel> personnelDAO = AbstractDAOFactory
+				.getFactory(dt) // Factory
+				.getPersonnelDAO();
+		DAO<PersonnelGroupe> pgDAO = AbstractDAOFactory
+				.getFactory(dt)
+				.getPersonnelGroupeDAO();
+		// Creation personnel
+		Personnel dupond = new PersonnelBuilder("youyou", "youcef", "Employé")
+				   			.build();
+		Personnel dupont = new PersonnelBuilder("bachir", "skakni", "Employé")
+							.build();
+		PersonnelGroupe pg = new PersonnelGroupe();
+		PersonnelGroupe spg = new PersonnelGroupe();
+		pg.addPersonnel(spg);
+		pg.addPersonnel(dupont);
+		spg.addPersonnel(dupond);
+		// Ajout DAO
+		pgDAO.create(pg);
+		pgDAO.create(spg);
+		personnelDAO.create(dupond); // update
+		personnelDAO.create(dupont); // update
+		System.out.println("\t" + pgDAO.read(spg.getId()));
+		System.out.println("\t" + personnelDAO.read("Dupont"));
 	}
-
-
-	    public static void main(final String[] args) {
-	     APPLICATION.run(args);
-	    }
+	
+	public static void main(String[] args) {
+		JdbcInitializer jinit = new JdbcInitializer();
+		jinit.dropCreateTables(); 
+		ENVIRONNEMENT.run(args, DAOType.JDBC);
+	}
 }
